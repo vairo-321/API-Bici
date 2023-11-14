4eeb0bd09a4fc800150a0d251f3ac060721cc361
esteban <diazesteban@gmail.com>
2023-11-14 01:45:50 -0300

    Proyecto inicial

---
+++ src/main/java/utnfrc/backend/ApiBiciApplication.java
@@ -0,0 +1,13 @@
+package utnfrc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBiciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBiciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utnfrc/backend/ApiBiciApplicationTests.java
@@ -0,0 +1,13 @@
+package utnfrc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBiciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
